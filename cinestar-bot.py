b355e7413f4bdee6262201906e2e86b3122cf7ab
Simon Padberg <simon.padberg@gmx.de>
2022-05-08 22:37:54 +0100

    Added E-Mail feature with some formatting

--- cinestar-bot.py
+++ cinestar-bot.py
@@ -1,12 +1,12 @@
 import requests
+import urllib.request
 from pyquery import PyQuery
+from redmail import outlook
+
+from settings import SMTP_USER, SMTP_PASS, RECEIVERS
 
 # Location doesn't matter. Movies of the week are same everywhere
-URL = 'https://www.cinestar.de/kino-bremen-kristall-palast/film-der-woche'
-
-TELEGRAM_CHANNEL_ID = '<THE CHANNEL ID>'
-TELEGRAM_BOT_TOKEN = '<THE BOT TOKEN>'
-
+URL = "https://www.cinestar.de/kino-bremen-kristall-palast/film-der-woche"
 
 class Movie:
     def __init__(self, movie_id, title, poster_url):
@@ -15,57 +15,36 @@ class Movie:
         self.poster_url = poster_url
 
 
-def send_to_telegram(text, image_url=None):
-    params = {
-        'chat_id': TELEGRAM_CHANNEL_ID,
-    }
-
-    method: str
-    if image_url:
-        method = 'sendPhoto'
-        params['caption'] = text
-        params['photo'] = image_url
-    else:
-        method = 'sendMessage'
-        params['text'] = text
-
-    requests.get(f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/{method}', params=params)
-
-
-def send_update_to_telegram(date, movies):
-    send_to_telegram(date)
-    for movie in movies:
-        send_to_telegram(movie.title, movie.poster_url)
-
-
 def query_movie_information(movie_id):
-    response = requests.get(f"https://www.cinestar.de/api/show/{movie_id}?appVersion=1.5.3")
+    response = requests.get(
+        f"https://www.cinestar.de/api/show/{movie_id}?appVersion=1.5.3"
+    )
     json = response.json()
-    title = json['title']
+    title = json["title"]
     # The URL from the API contains a quite small picture
     # We want a big one
-    poster_url = json['poster'].replace('/poster_tile/', '/web_l/')
+    poster_url = json["poster"].replace("/poster_tile/", "/web_l/")
     return Movie(movie_id, title, poster_url)
 
 
 def get_movies_of_the_week(webpage):
-    ids_text: str = webpage('[data-show-ids]')[0].attrib['data-show-ids']
-    ids = map(int, ids_text.split(','))
+    ids_text: str = webpage("[data-show-ids]")[0].attrib["data-show-ids"]
+    ids = map(int, ids_text.split(","))
     movies = map(query_movie_information, ids)
     return list(movies)
 
 
 def write_new_date(date):
-    with open('cinestar-date', 'w') as f:
+    with open("cinestar-date", "w") as f:
         return f.write(date)
 
 
 def get_last_date():
     try:
-        with open('cinestar-date', 'r') as f:
+        with open("cinestar-date", "r") as f:
             return f.read()
     except FileNotFoundError:
-        return ''
+        return ""
 
 
 def get_webpage():
@@ -74,8 +53,27 @@ def get_webpage():
 
 
 def get_date_text(webpage: PyQuery):
-    date_text = webpage('div.subHeadline')[0].text.strip()
+    date_text = webpage("div.subHeadline")[0].text.strip()
     return date_text
+
+
+def send_mail(movie: Movie, date: str, cover_file: str):
+    outlook.username = SMTP_USER
+    outlook.password = SMTP_PASS
+
+    outlook.send(
+        sender=f"Cinestar Bot <{outlook.username}>",
+        subject="Film der Woche",
+        receivers=RECEIVERS,
+        html=f"""
+        <h1>{movie.title}</h1>
+        <h2>{date}</h2>
+        {{{{ my_image }}}}
+    """,
+        body_images={
+            "my_image": f"{cover_file}",
+        },
+    )
 
 
 def main():
@@ -85,8 +83,12 @@ def main():
     if date != last_date:
         write_new_date(date)
         movies = get_movies_of_the_week(webpage)
-        send_update_to_telegram(date, movies)
+
+        for i, movie in enumerate(movies):
+            cover_file = f"movie-{i}.jpg"
+            urllib.request.urlretrieve(movie.poster_url, cover_file)
+            send_mail(movie, date, cover_file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
